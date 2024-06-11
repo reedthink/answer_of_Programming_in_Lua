b82ae74ae33c242ca5ce814fd3eca9fe696a1855
jkjk <reedthink@qq.com>
2024-06-11 13:34:45 +0800

    添加闭包例子

--- closure.lua
+++ closure.lua
@@ -30,3 +30,23 @@ print(c2())
 
 
 print(c1())
+
+-- 好像很平滑就理解了函数是第一类值
+-- C++那种才不方便理解，因为一开始的抽象层级没有充分屏蔽底层细节
+function F(x)
+    return {
+        set = function(y) x = y end,
+        get = function()
+            return x
+        end
+    }
+end
+
+o1 = F(10)
+o2 = F(20)
+
+print(o1.get(), o2.get())
+
+o2.set(100)
+o1.set(300)
+print(o1.get(), o2.get())
